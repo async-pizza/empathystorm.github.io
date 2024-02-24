4da85b0deb35267c3d8318cb37874173dc7c2720
StrangePineAplle <shadow77777777@mail.ru>
2024-02-24 14:22:01 +0300

    bloodCenters upd

--- giveMoney/app.js
+++
@@ -1,7 +0,0 @@
-let donation = document.getElementById("donation");
-let top = document.getElementById("top");
-let blood = document.getElementById("blood");
-let money = document.getElementById("money");
-
-
-
